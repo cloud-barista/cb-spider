0c6f4abc03525d480f0fdabb3cf09fcd5732fa1d
powerkim <powerkim@etri.re.kr>
2019-10-09 01:31:55 +0900

    driver interface upgrade for resources

---
+++ cloud-control-manager/cloud-driver/interfaces/new-resources/SecurityHandler.go
@@ -0,0 +1,37 @@
+// Cloud Driver Interface of CB-Spider.
+// The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
+// The CB-Spider Mission is to connect all the clouds with a single interface.
+//
+//      * Cloud-Barista: https://github.com/cloud-barista
+//
+// This is Resouces interfaces of Cloud Driver.
+//
+// by powerkim@etri.re.kr, 2019.06.
+
+package resources
+
+type SecurityReqInfo struct {
+     Name string
+     SecurityRules *[]SecurityRuleInfo
+}
+
+type SecurityRuleInfo struct {
+        FromPort   string
+        ToPort     string
+        IPProtocol string
+        Direction string
+}
+
+type SecurityInfo struct {
+     Name string
+     SecurityRules *[]SecurityRuleInfo
+
+     keyValueList []KeyValue
+}
+
+type SecurityHandler interface {
+	CreateSecurity(securityReqInfo SecurityReqInfo) (SecurityInfo, error)
+	ListSecurity() ([]*SecurityInfo, error)
+	GetSecurity(securityID string) (SecurityInfo, error)
+	DeleteSecurity(securityID string) (bool, error)
+}
