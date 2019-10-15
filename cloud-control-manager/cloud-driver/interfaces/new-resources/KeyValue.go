b74dfe4ba26a4bdc67ca51ac3857f34a111646a8
jazmandorf <thaeao@Jazui-MacBookPro.local>
2019-10-15 11:34:56 +0900

    add new resource at interface

---
+++ cloud-control-manager/cloud-driver/interfaces/new-resources/KeyValue.go
@@ -0,0 +1,16 @@
+// Cloud Driver Interface of CB-Spider.
+// The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
+// The CB-Spider Mission is to connect all the clouds with a single interface.
+//
+//      * Cloud-Barista: https://github.com/cloud-barista
+//
+// This is Resouces interfaces of Cloud Driver.
+//
+// by powerkim@etri.re.kr, 2019.10.
+
+package resources
+
+type KeyValue struct {
+        Key string
+        Value string
+}
