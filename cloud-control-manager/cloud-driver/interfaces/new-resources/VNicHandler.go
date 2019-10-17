4b9740c61a947cf18854d26accd5a9265a12ceeb
Jihoon Seo <46767780+jihoon-seo@users.noreply.github.com>
2019-10-17 10:35:42 +0900

    Fix typo (MacAdress -> MacAddress)

--- cloud-control-manager/cloud-driver/interfaces/new-resources/VNicHandler.go
+++ cloud-control-manager/cloud-driver/interfaces/new-resources/VNicHandler.go
@@ -21,7 +21,7 @@ type VNicInfo struct {
 	Id               string
 	Name             string
 	PublicIP         string
-	MacAdress        string
+	MacAddress        string
 	OwnedVMID        string
 	SecurityGroupIds []string
 	Status           string
