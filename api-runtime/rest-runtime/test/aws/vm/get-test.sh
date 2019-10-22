9875452b14aa01438bcc0df5ec70973337c6d644
devunet <devunet@mz.co.kr>
2019-10-22 20:04:37 +0900

    VM & VNetwork 테스트 완료

--- api-runtime/rest-runtime/test/aws/vm/get-test.sh
+++ api-runtime/rest-runtime/test/aws/vm/get-test.sh
@@ -1,3 +1,5 @@
 RESTSERVER=localhost
 
-curl -X GET http://$RESTSERVER:1024/vm/vm01?connection_name=aws-config01 |json_pp
+#정상 동작
+#id로 조회
+curl -X GET http://$RESTSERVER:1024/vm/i-075d740aaaa410193?connection_name=aws-config01 |json_pp
