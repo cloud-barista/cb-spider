fcfc97e16a6f574e08ebc5822e81798bd6f45030
jazmandorf <jazmandorf@gmail.com>
2019-10-30 16:02:20 +0900

    modified gcp VmHandler & test script

--- api-runtime/rest-runtime/test/gcp/vm/terminate-test.sh
+++ api-runtime/rest-runtime/test/gcp/vm/terminate-test.sh
@@ -1,4 +1,4 @@
 RESTSERVER=localhost
 
-#서버 에러 발생 - "message" : "Method Not Allowed"
-curl -X DELETE http://$RESTSERVER:1024/vm/cscmcloud?connection_name=gcp-config01 |json_pp
+#완료
+curl -X DELETE http://$RESTSERVER:1024/vm/vm01?connection_name=gcp-config01 |json_pp
