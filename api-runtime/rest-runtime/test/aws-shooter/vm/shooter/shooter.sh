6e8f0433b6c998c54965b25d72ba55e019bbd011
Jihoon Seo <jihoon.seo@etri.re.kr>
2019-11-04 14:47:44 +0900

    Update AWS Shooter shell scripts

--- api-runtime/rest-runtime/test/aws-shooter/vm/shooter/shooter.sh
+++ api-runtime/rest-runtime/test/aws-shooter/vm/shooter/shooter.sh
@@ -1,4 +1,5 @@
-SERVER=52.194.71.58
+#!/bin/bash
+SERVER=52.8.176.204
 
 
 HN=`hostname`
@@ -7,6 +8,6 @@ while :
 do
 	DT=`date`
 	DT=`echo $DT |sed 's/ /%20/g'`
-	curl -X GET http://$SERVER:119/test -H 'Content-Type: application/json' -d '{"Date": "'${DT}'", "HostName": "'${HN}'"}'
+	curl -sX GET http://$SERVER:119/test -H 'Content-Type: application/json' -d '{"Date": "'${DT}'", "HostName": "'${HN}'"}'
 	sleep 5
 done
