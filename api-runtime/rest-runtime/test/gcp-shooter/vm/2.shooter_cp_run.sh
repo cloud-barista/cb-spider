b4ed1ced75b9297eb9d9f273c3b763d04a5046e5
powerkim <powerkim@etri.re.kr>
2019-11-12 14:16:24 +0900

    gcp shooter release

--- api-runtime/rest-runtime/test/gcp-shooter/vm/2.shooter_cp_run.sh
+++ api-runtime/rest-runtime/test/gcp-shooter/vm/2.shooter_cp_run.sh
@@ -6,7 +6,7 @@ KEY_NAME=${CONNECT_NAMES[0]}
 num=0
 for NAME in "${CONNECT_NAMES[@]}"
 do
-	if [ $num -eq -1 ] ; then
+	if [ $num -eq 0 ] ; then
 		echo .... first vm skipped!!
 	else
 		echo ========================== $NAME
@@ -15,8 +15,8 @@ do
 		do
 			echo $NAME: copy shooter into ${PUBLIC_IP} ...
 			ssh-keygen -f "/root/.ssh/known_hosts" -R ${PUBLIC_IP}
-			scp -i ../keypair/$KEY_NAME.key -o "StrictHostKeyChecking no" ./shooter/shooter.sh powerkim-prj@$PUBLIC_IP:/tmp
-			ssh -i ../keypair/$KEY_NAME.key -o "StrictHostKeyChecking no" powerkim-prj@$PUBLIC_IP /tmp/shooter.sh &
+			scp -i ../keypair/$KEY_NAME.key -o "StrictHostKeyChecking no" ./shooter/shooter.sh cb-user@$PUBLIC_IP:/tmp
+			ssh -i ../keypair/$KEY_NAME.key -o "StrictHostKeyChecking no" cb-user@$PUBLIC_IP /tmp/shooter.sh &
 		done
 		
 	fi
