45800e3459c9704c6a2a1857abd229e5106467a9
hyokyung.kim <hyokyung.kim@innogrid.com>
2019-11-20 17:01:47 +0900

    - Cloudit Security, VNetwork NameID 조회 시 not found 예외 처리 추가

--- cloud-control-manager/cloud-driver/drivers/cloudit/resources/SecurityHandler.go
+++ cloud-control-manager/cloud-driver/drivers/cloudit/resources/SecurityHandler.go
@@ -114,28 +114,13 @@ func (securityHandler *ClouditSecurityHandler) ListSecurity() ([]*irs.SecurityIn
 }
 
 func (securityHandler *ClouditSecurityHandler) GetSecurity(securityNameID string) (irs.SecurityInfo, error) {
-	var securityInfo *irs.SecurityInfo
-
-	securityList, err := securityHandler.ListSecurity()
+	// 이름 기준 보안그룹 조회
+	securityInfo, err := securityHandler.getSecurityByName(securityNameID)
 	if err != nil {
-		return irs.SecurityInfo{}, nil
-	}
-	for _, s := range securityList {
-		if strings.EqualFold(s.Name, securityNameID) {
-			securityInfo = s
-			break
-		}
-	}
-
-	if securityInfo == nil {
-		err := errors.New(fmt.Sprintf("failed to find security group with name %s", securityNameID))
+		cblogger.Error(err)
 		return irs.SecurityInfo{}, err
 	}
-	return *securityInfo, nil
-}
 
-/* 지워도되는 코드
-func (securityHandler *ClouditSecurityHandler) GetSecurity(securityID string) (irs.SecurityInfo, error) {
 	securityHandler.Client.TokenID = securityHandler.CredentialInfo.AuthToken
 	authHeader := securityHandler.Client.AuthenticatedHeaders()
 
@@ -143,31 +128,34 @@ func (securityHandler *ClouditSecurityHandler) GetSecurity(securityID string) (i
 		MoreHeaders: authHeader,
 	}
 
-	if securityInfo, err := securitygroup.Get(securityHandler.Client, securityID, &requestOpts); err != nil {
+	// SecurityGroup Rule 정보 가져오기
+	if sgRules, err := securitygroup.ListRule(securityHandler.Client, securityInfo.ID, &requestOpts); err != nil {
 		return irs.SecurityInfo{}, err
 	} else {
-		// SecurityGroup Rule 정보 가져오기
-		if sgRules, err := securitygroup.ListRule(securityHandler.Client, securityInfo.ID, &requestOpts); err != nil {
-			return irs.SecurityInfo{}, err
-		} else {
-			(*securityInfo).Rules = *sgRules
-			(*securityInfo).RulesCount = len(*sgRules)
-		}
-		spew.Dump(securityInfo)
-		secGroupInfo := setterSecGroup(*securityInfo)
-		return *secGroupInfo, nil
+		(*securityInfo).Rules = *sgRules
+		(*securityInfo).RulesCount = len(*sgRules)
 	}
-}*/
+	//spew.Dump(securityInfo)
+	secGroupInfo := setterSecGroup(*securityInfo)
+	return *secGroupInfo, nil
+}
 
-func (securityHandler *ClouditSecurityHandler) DeleteSecurity(securityID string) (bool, error) {
+func (securityHandler *ClouditSecurityHandler) DeleteSecurity(securityNameID string) (bool, error) {
+	// 이름 기준 보안그룹 조회
+	securityInfo, err := securityHandler.getSecurityByName(securityNameID)
+	if err != nil {
+		cblogger.Error(err)
+		return false, err
+	}
+
 	securityHandler.Client.TokenID = securityHandler.CredentialInfo.AuthToken
 	authHeader := securityHandler.Client.AuthenticatedHeaders()
 
 	requestOpts := client.RequestOpts{
 		MoreHeaders: authHeader,
 	}
-
-	if err := securitygroup.Delete(securityHandler.Client, securityID, &requestOpts); err != nil {
+	// 보안그룹 삭제
+	if err := securitygroup.Delete(securityHandler.Client, securityInfo.ID, &requestOpts); err != nil {
 		return false, err
 	} else {
 		return true, nil
@@ -188,4 +176,33 @@ func (securityHandler *ClouditSecurityHandler) CheckSecurityExist(securityName s
 		}
 	}
 	return &securityId, nil
+}
+
+func (securityHandler *ClouditSecurityHandler) getSecurityByName(securityName string) (*securitygroup.SecurityGroupInfo, error) {
+	var security *securitygroup.SecurityGroupInfo
+
+	securityHandler.Client.TokenID = securityHandler.CredentialInfo.AuthToken
+	authHeader := securityHandler.Client.AuthenticatedHeaders()
+
+	requestOpts := client.RequestOpts{
+		MoreHeaders: authHeader,
+	}
+
+	securityList, err := securitygroup.List(securityHandler.Client, &requestOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, s := range *securityList {
+		if strings.EqualFold(s.Name, securityName) {
+			security = &s
+			break
+		}
+	}
+
+	if security == nil {
+		err := errors.New(fmt.Sprintf("failed to find security group with name %s", securityName))
+		return nil, err
+	}
+	return security, nil
 }
