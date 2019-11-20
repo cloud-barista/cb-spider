21780cc8c57e630e9fe96f23004fd747dd842291
hyokyung.kim <hyokyung.kim@innogrid.com>
2019-11-20 16:59:33 +0900

    - Cloudit ImageHandler GetImage() NameId 적용

--- api-runtime/rest-runtime/test/cloudit/image/get-test.sh
+++ api-runtime/rest-runtime/test/cloudit/image/get-test.sh
@@ -1,4 +1,4 @@
 RESTSERVER=localhost
 
-IMAGE_ID=a846af3b-5d80-4182-b38e-5501ad9f78f4
+IMAGE_ID=CentOS-7
 curl -X GET http://$RESTSERVER:1024/vmimage/$IMAGE_ID?connection_name=cloudit-config01 | json_pp
