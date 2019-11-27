70ecee5a15d4d510781709cbfaa2d63d9e06dd3a
hyokyung.kim <hyokyung.kim@innogrid.com>
2019-11-27 14:15:27 +0900

    OpenStack, Cloudit start-test 쉘 스크립트 설명 주석 추가

--- api-runtime/rest-runtime/test/cloudit/vm/start-test.sh
+++ api-runtime/rest-runtime/test/cloudit/vm/start-test.sh
@@ -6,7 +6,7 @@ RESTSERVER=localhost
 
 # Cloudit VM 생성 테스트 시 리소스 이름 정보
 
-# ImageId = CentOS 7.6
+# ImageId = CentOS-7
 # VirtualNetworkId(서브넷 ID와 매핑) = CB-Subnet
 # SecurityGroupIds = CB-SecGroup
 # VMSpecId = micro-1
@@ -14,24 +14,12 @@ RESTSERVER=localhost
 # Cloudit의 경우 KeyPair 미지원 (root 계정 패스워드 로그인 방식 지원)
 # VMUserPasswd = "etriETRI!@"
 
-#curl -X POST http://$RESTSERVER:1024/vm?connection_name=cloudit-config01 -H 'Content-Type: application/json' -d '{
-#    "VMName": "CBVm",
-#    "ImageId": "2813a6ed-66f8-4041-9cfa-2cb5cb416520",
-#    "VirtualNetworkId": "10.0.4.0",
-#    "SecurityGroupIds": [
-#        "064616d0-74fe-4840-8a42-8af4ce24e96a"
-#    ],
-#    "VMSpecId": "1c38e438-ede9-4df5-8775-2ce791698924",
-#    "VMUserPasswd": "etriETRI!@"
-#}' |json_pp
-
 curl -X POST http://$RESTSERVER:1024/vm?connection_name=cloudit-config01 -H 'Content-Type: application/json' -d '{
     "VMName": "CBVm",
     "ImageId": "CentOS-7",
     "VirtualNetworkId": "CB-Subnet",
     "SecurityGroupIds": [
-        "CB-SecGroup",
-        "ALL"
+        "CB-SecGroup"
     ],
     "VMSpecId": "micro-1",
     "VMUserPasswd": "etriETRI!@"
--- api-runtime/rest-runtime/test/openstack/vm/start-test.sh
+++ api-runtime/rest-runtime/test/openstack/vm/start-test.sh
@@ -3,10 +3,10 @@ RESTSERVER=localhost
 # OpenStack VM 생성 테스트 시 리소스 이름 정보
 
 # ImageId = ubuntu16.04
-# VirtualNetworkId(서브넷 ID와 매핑) = CB-Subnet
+# VirtualNetworkId(Not Required) : 내부적으로 CB-VNet (가상 네트워크) ID 설정
 # SecurityGroupIds = CB-SecGroup
 # VMSpecId = m1.small
-# KeyPairName(Name이 ID 처럼 사용) =  CB-KeyPair
+# KeyPairName = CB-KeyPair
 
 curl -X POST http://$RESTSERVER:1024/vm?connection_name=openstack-config01 -H 'Content-Type: application/json' -d '{
     "VMName": "CBVm2",
