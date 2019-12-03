47391e01a9668a43412fbb492c257fc2fd877eab
devunet <devunet@mz.co.kr>
2019-12-03 17:43:52 +0900

    Test스크립트 NameId 기반으로 변경 및 테스트 완료 VNetwork - Name기반 수정및 테스트 완료 KeyPair - Name기반 수정및 테스트 완료 Security - Name기반 수정및 테스트 완료 PublicIp - Name기반 수정및 테스트 완료 VM - Name기반 수정및 테스트 완료 Vnic - ID 기반이라 핸들러 수정 필요

--- api-runtime/rest-runtime/test/aws/vm/terminate-test.sh
+++ api-runtime/rest-runtime/test/aws/vm/terminate-test.sh
@@ -1,4 +1,5 @@
 RESTSERVER=localhost
 
 
-curl -X DELETE "http://$RESTSERVER:1024/vm/i-03665b7acbb9f623a?connection_name=aws-config01"
+#정상 동작
+curl -X DELETE "http://$RESTSERVER:1024/vm/cbvm01?connection_name=aws-config01"
