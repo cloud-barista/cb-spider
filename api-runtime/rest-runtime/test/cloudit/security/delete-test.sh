0645f2d7c2bd1883f07e26179561bc2cd9305812
hyokyung.kim <hyokyung.kim@innogrid.com>
2019-12-04 21:10:34 +0900

    Cloudit 드라이버 REST API 쉘 스크립트 테스트 및 설명 추가

--- api-runtime/rest-runtime/test/cloudit/security/delete-test.sh
+++ api-runtime/rest-runtime/test/cloudit/security/delete-test.sh
@@ -1,4 +1,4 @@
 RESTSERVER=localhost
 
-SECURITY_ID=6a3bcb31-c172-43ba-a7d0-4ae17dcf74bd
-curl -X DELETE http://$RESTSERVER:1024/securitygroup/$SECURITY_ID?connection_name=cloudit-config01 | json_pp &
+SECURITY_NAME=CB-SecGroup
+curl -X DELETE http://$RESTSERVER:1024/securitygroup/$SECURITY_NAME?connection_name=cloudit-config01 | json_pp
--- api-runtime/rest-runtime/test/cloudit/security/get-test.sh
+++ api-runtime/rest-runtime/test/cloudit/security/get-test.sh
@@ -1,5 +1,4 @@
 RESTSERVER=localhost
 
 SECURITY_NAME=CB-SecGroup
-#curl -X GET http://$RESTSERVER:1024/securitygroup/$SECURITY_ID?connection_name=cloudit-config01 | json_pp
 curl -X GET http://$RESTSERVER:1024/securitygroup/$SECURITY_NAME?connection_name=cloudit-config01 | json_pp
