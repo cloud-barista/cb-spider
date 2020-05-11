a625b5d6be6707af359f286863d59c7b63f2c9a7
devunet <devunet@mz.co.kr>
2020-05-11 22:18:27 +0900

    VPC 생성 후 리턴 정보중 일부 버그 수정(System Id / Key Value)

--- cloud-control-manager/cloud-driver/drivers/gcp/main/Test_Resources.go
+++ cloud-control-manager/cloud-driver/drivers/gcp/main/Test_Resources.go
@@ -341,7 +341,7 @@ func handleVPC() {
 	handler := ResourceHandler.(irs.VPCHandler)
 
 	vpcReqInfo := irs.VPCReqInfo{
-		IId: irs.IID{NameId: "cb-vpc"},
+		IId: irs.IID{NameId: "cb-vpc2"},
 		//IPv4_CIDR: "10.0.0.0/16",
 		SubnetInfoList: []irs.SubnetInfo{
 			{
@@ -855,11 +855,11 @@ func handleVM() {
 
 func main() {
 	cblogger.Info("GCP Resource Test")
-	//handleVPC()
+	handleVPC()
 	//handleVMSpec()
 	//handleImage() //AMI
 	//handleKeyPair()
 	//handleSecurity()
 
-	handleVM()
+	//handleVM()
 }
