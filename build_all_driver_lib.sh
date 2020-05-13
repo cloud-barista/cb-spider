0c885cce06a5543e4303063ceca73075a0a2111b
Jihoon Seo <jihoon.seo@etri.re.kr>
2020-05-13 20:13:51 +0900

    Update build_driver scripts

--- build_all_driver_lib.sh
+++ build_all_driver_lib.sh
@@ -8,12 +8,16 @@ DRIVERS=( aws azure openstack gcp alibaba docker)
 DRIVER_PATH=$CBSPIDER_ROOT/cloud-control-manager/cloud-driver/drivers
 DRIVERLIB_PATH=$CBSPIDER_ROOT/cloud-driver-libs
 
-num=0
-for DRIVER in "${DRIVERS[@]}"
-do
-        echo  ============ build ${DRIVER} ... ============
-        DRIVER_BUILD_SHELL=$DRIVER_PATH/$DRIVER/build_driver_lib.sh
-        /bin/bash -c 'cd '$DRIVER_PATH/$DRIVER';'$DRIVER_BUILD_SHELL'' 
+function build() {
+        num=0
+        for DRIVER in "${DRIVERS[@]}"
+        do
+                echo  ============ build ${DRIVER} ... ============
+                DRIVER_BUILD_SHELL=$DRIVER_PATH/$DRIVER/build_driver_lib.sh
+                /bin/bash -c 'cd '$DRIVER_PATH/$DRIVER';'$DRIVER_BUILD_SHELL'' || return 1
 
-        num=`expr $num + 1`
-done
+                num=`expr $num + 1`
+        done
+        }
+
+build
