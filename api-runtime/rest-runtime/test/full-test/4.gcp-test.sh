e1a42d336604800cae0ae499142cb2c15733d43a
powerkim <powerkim@etri.re.kr>
2020-05-14 11:07:57 +0900

    changed image NameId type into URL format

--- api-runtime/rest-runtime/test/full-test/4.gcp-test.sh
+++ api-runtime/rest-runtime/test/full-test/4.gcp-test.sh
@@ -1,5 +1,7 @@
 export CONN_CONFIG=gcp-ohio-config
-export IMAGE_NAME=ubuntu-1804-bionic-v20191002
+#export IMAGE_NAME=ubuntu-minimal-1804-bionic-v20191024
+#export IMAGE_NAME=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-1804-bionic-v20191024
+export IMAGE_NAME=https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-minimal-1804-bionic-v20191024
 export SPEC_NAME=f1-micro
 
 ./full_test.sh
