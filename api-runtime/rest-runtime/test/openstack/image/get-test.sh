193506b32846a76b330a8fd23793120c952653ae
powerkim <powerkim@etri.re.kr>
2020-05-14 15:41:42 +0900

    clear unused test scripts

--- api-runtime/rest-runtime/test/openstack/image/get-test.sh
+++
@@ -1,7 +0,0 @@
-RESTSERVER=localhost
-
-# OpenStack의 경우 사용자가 등록한 이미지 정보를 조회
-# ubuntu16.04 이름을 기준으로 이미지 정보 조회
-
-IMAGE_ID=ubuntu16.04
-curl -X GET http://$RESTSERVER:1024/spider/vmimage/$IMAGE_ID?connection_name=openstack-config01 |json_pp
--- api-runtime/rest-runtime/test/openstack/vm/list-test.sh
+++
@@ -1,3 +0,0 @@
-RESTSERVER=localhost
-
-curl -X GET http://$RESTSERVER:1024/spider/vm?connection_name=openstack-config01 |json_pp
