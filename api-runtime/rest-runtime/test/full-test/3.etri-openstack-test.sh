e05a8e415d774172340e280146921996d9649ee9
powerkim <powerkim@etri.re.kr>
2020-05-14 15:02:03 +0900

    add test scripts and more

--- api-runtime/rest-runtime/test/full-test/3.etri-openstack-test.sh
+++ api-runtime/rest-runtime/test/full-test/3.etri-openstack-test.sh
@@ -1,8 +1,5 @@
 export CONN_CONFIG=openstack-config01
-#export IMAGE_NAME=Ubuntu16.04_2
 export IMAGE_NAME=Ubuntu-18.04
-#export SPEC_NAME=nano.1
 export SPEC_NAME=m1.tiny
 
-
-./all_delete.sh
+./full_test.sh
--- build_all_driver_lib.sh
+++ build_all_driver_lib.sh
@@ -3,7 +3,7 @@ source setup.env
 
 #DRIVERS=( aws azure cloudit gcp openstack cloudtwin )
 #DRIVERS=( aws azure cloudit gcp openstack alibaba)
-DRIVERS=( aws azure openstack gcp alibaba docker)
+DRIVERS=( aws azure openstack gcp alibaba cloudit docker)
 
 DRIVER_PATH=$CBSPIDER_ROOT/cloud-control-manager/cloud-driver/drivers
 DRIVERLIB_PATH=$CBSPIDER_ROOT/cloud-driver-libs
