9550d34466030d228054988ba2548ab0a5f78736
powerkim <powerkim@etri.re.kr>
2020-05-29 22:25:49 +0900

    modify test scripts of ETRI OpenStack

--- api-runtime/rest-runtime/test/full-test/3.etri-openstack-test.sh
+++ api-runtime/rest-runtime/test/full-test/3.etri-openstack-test.sh
@@ -1,5 +1,5 @@
 export CONN_CONFIG=openstack-config01
-export IMAGE_NAME=Ubuntu-18.04
-export SPEC_NAME=m1.tiny
+export IMAGE_NAME=ubuntu-18.04
+export SPEC_NAME=m1.small
 
 ./full_test.sh
--- api-runtime/rest-runtime/test/full-test/clear-tools/3.openstack-test.sh
+++ api-runtime/rest-runtime/test/full-test/clear-tools/3.openstack-test.sh
@@ -1,8 +1,8 @@
 export CONN_CONFIG=openstack-config01
 #export IMAGE_NAME=Ubuntu16.04_2
-export IMAGE_NAME=Ubuntu-18.04
+export IMAGE_NAME=ubuntu-18.04
 #export SPEC_NAME=nano.1
-export SPEC_NAME=m1.tiny
+export SPEC_NAME=m1.small
 
 
 ./all_delete.sh
