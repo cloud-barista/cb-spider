53fe394df2649e9348bee7bfa3770a32079419c2
powerkim <powerkim@etri.re.kr>
2020-06-25 20:35:54 +0900

    AdminWeb: fix the error of resource deletion

---
+++ api-runtime/rest-runtime/admin-web/AdminWeb-CCM.go
@@ -0,0 +1,35 @@
+// Cloud Info Manager's Rest Runtime of CB-Spider.
+// The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
+// The CB-Spider Mission is to connect all the clouds with a single interface.
+//
+//      * Cloud-Barista: https://github.com/cloud-barista
+//
+// by CB-Spider Team, 2020.06.
+
+package adminweb
+
+import (
+//	"fmt"
+/*
+        "github.com/cloud-barista/cb-store/config"
+        "github.com/sirupsen/logrus"
+	cr "github.com/cloud-barista/cb-spider/api-runtime/common-runtime"
+	dim "github.com/cloud-barista/cb-spider/cloud-info-manager/driver-info-manager"
+	cim "github.com/cloud-barista/cb-spider/cloud-info-manager/credential-info-manager"
+	rim "github.com/cloud-barista/cb-spider/cloud-info-manager/region-info-manager"
+	ccim "github.com/cloud-barista/cb-spider/cloud-info-manager/connection-config-info-manager"
+	"strconv"
+
+	"net/http"
+	"io/ioutil"
+	"strings"
+	"github.com/labstack/echo"
+	"encoding/json"
+*/
+)
+
+func VMSpec() {
+	cblog.Info("call VMSpec()")
+
+}
+
