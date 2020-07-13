fdaa1fd9fb06710faa322f5917be9aba8af263c3
Sean Oh <innodreamer@gmail.com>
2020-07-13 15:36:43 +0900

    Create and Update SSH Run test files

--- api-runtime/rest-runtime/test/vm-ssh/sshrunkeypath-test.sh
+++ api-runtime/rest-runtime/test/vm-ssh/sshrunkeypath-test.sh
@@ -2,7 +2,7 @@ RESTSERVER=localhost
 
 curl -X POST http://$RESTSERVER:1024/sshrunkeypath -H 'Content-Type: application/json' -d '{
     "UserName": "ubuntu",
-    "KeyPath"  : "/home/sean/.ssh/keyfile.pem",
-    "ServerPort": "18.163.97.170:22",
+    "KeyPath"  : "/home/ubuntu/.ssh/keyfile.pem",
+    "ServerPort": "18.163.00.000:22",
     "Command": "/bin/hostname"
 }'
