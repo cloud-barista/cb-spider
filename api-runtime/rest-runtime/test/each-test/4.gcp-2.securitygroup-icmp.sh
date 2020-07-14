f2ee904bb874012d6db91d3b71d9da1526028fa6
powerkim <powerkim@etri.re.kr>
2020-07-14 15:55:46 +0900

    change the ImageName of GCP Test Scripts

--- api-runtime/rest-runtime/test/each-test/4.gcp-2.securitygroup-icmp.sh
+++ api-runtime/rest-runtime/test/each-test/4.gcp-2.securitygroup-icmp.sh
@@ -1,5 +1,6 @@
 export CONN_CONFIG=gcp-ohio-config
-export IMAGE_NAME=ubuntu-1804-bionic-v20191002
+#export IMAGE_NAME=ubuntu-1804-bionic-v20191002
+export IMAGE_NAME=https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-minimal-1804-bionic-v20191024
 export SPEC_NAME=f1-micro
 
 ./securitygroup-icmp-test.sh
