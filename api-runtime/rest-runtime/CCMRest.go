ecc643e9f7b238ee427712da75e8e7523dde91fe
powerkim <powerkim@etri.re.kr>
2020-08-25 08:54:43 +0900

    header typo modification

--- api-runtime/rest-runtime/CCMRest.go
+++ api-runtime/rest-runtime/CCMRest.go
@@ -1,4 +1,4 @@
-// Cloud Control Manager's Rest Runtime of CB-Spider.ll
+// Cloud Control Manager's Rest Runtime of CB-Spider.
 // The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
 // The CB-Spider Mission is to connect all the clouds with a single interface.
 //
