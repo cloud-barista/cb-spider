a030c9fad308d7881d38b3b9996324fe61201bc8
powerkimhub <powerkim@etri.re.kr>
2020-10-28 15:03:04 +0900

    clear unused codes

--- api-runtime/apiserver.go
+++ api-runtime/apiserver.go
@@ -5,9 +5,7 @@ import (
 
 	// cr "github.com/cloud-barista/cb-spider/api-runtime/common-runtime"
 	grpcruntime "github.com/cloud-barista/cb-spider/api-runtime/grpc-runtime"
-	restruntime "github.com/cloud-barista/cb-spider/api-runtime/rest-runtime"
-	_ "time"
-	_ "fmt"
+	restruntime "github.com/cloud-barista/cb-spider/api-runtime/rest-runtime"	
 )
 
 func main() {
@@ -25,16 +23,5 @@ func main() {
 		wg.Done()
 	}()
 
-//	printAdminWebURL()
 	wg.Wait()
 }
-/*
-func printAdminWebURL() {
-	for count:=0; count < 3; count++{
-		if cr.ServicePort
-		time.Sleep(100 * time.Millisecond)
-	}
-	adminWebURL := "http://" + cr.HostIPorName + cr.ServicePort + "/spider/adminweb"
-	fmt.Printf("\n   AdminWeb: %s\n\n", adminWebURL)
-}
-*/
