a47354e2fb6f442348a6c1054acfc73f367982f5
dev4unet <dev4unet@naver.com>
2021-05-24 11:22:27 +0900

    Tencent VMSpecHandler 완료  - Spec 검색 조건은 Region이 아닌 Zone임.

--- cloud-control-manager/cloud-driver/drivers/tencent/resources/VMSpecHandler.go
+++ cloud-control-manager/cloud-driver/drivers/tencent/resources/VMSpecHandler.go
@@ -18,8 +18,9 @@ type TencentVmSpecHandler struct {
 	Client *cvm.Client
 }
 
+//@TODO : Region : zone id(Region이 아닌 zone id로 조회해야 함.)
 func (vmSpecHandler *TencentVmSpecHandler) ListVMSpec(Region string) ([]*irs.VMSpecInfo, error) {
-	cblogger.Infof("Start ListVMSpec(Region:[%s])", Region)
+	cblogger.Infof("ListVMSpec(ZoneId:[%s])", Region)
 
 	callogger := call.GetLogger("HISCALL")
 	callLogInfo := call.CLOUDLOGSCHEMA{
@@ -67,7 +68,7 @@ func (vmSpecHandler *TencentVmSpecHandler) ListVMSpec(Region string) ([]*irs.VMS
 }
 
 func (vmSpecHandler *TencentVmSpecHandler) GetVMSpec(Region string, Name string) (irs.VMSpecInfo, error) {
-	cblogger.Infof("Start GetVMSpec(Region:[%s], Name:[%s])", Region, Name)
+	cblogger.Infof("Start GetVMSpec(ZoneId:[%s], Name:[%s])", Region, Name)
 
 	callogger := call.GetLogger("HISCALL")
 	callLogInfo := call.CLOUDLOGSCHEMA{
@@ -112,13 +113,95 @@ func (vmSpecHandler *TencentVmSpecHandler) GetVMSpec(Region string, Name string)
 }
 
 func (vmSpecHandler *TencentVmSpecHandler) ListOrgVMSpec(Region string) (string, error) {
-	cblogger.Infof("Start ListOrgVMSpec(Region:[%s])", Region)
-	return "", nil
+	cblogger.Infof("ListOrgVMSpec(ZoneId:[%s])", Region)
+
+	callogger := call.GetLogger("HISCALL")
+	callLogInfo := call.CLOUDLOGSCHEMA{
+		CloudOS:      call.TENCENT,
+		RegionZone:   vmSpecHandler.Region.Zone,
+		ResourceType: call.VMSPEC,
+		ResourceName: "ListOrgVMSpec()",
+		CloudOSAPI:   "DescribeInstanceTypeConfigs()",
+		ElapsedTime:  "",
+		ErrorMSG:     "",
+	}
+
+	request := cvm.NewDescribeInstanceTypeConfigsRequest()
+	request.Filters = []*cvm.Filter{
+		&cvm.Filter{
+			Name:   common.StringPtr("zone"),
+			Values: common.StringPtrs([]string{Region}),
+		},
+	}
+	callLogStart := call.Start()
+	response, err := vmSpecHandler.Client.DescribeInstanceTypeConfigs(request)
+	callLogInfo.ElapsedTime = call.Elapsed(callLogStart)
+
+	if err != nil {
+		callLogInfo.ErrorMSG = err.Error()
+		callogger.Error(call.String(callLogInfo))
+
+		cblogger.Error(err)
+		return "", err
+	}
+
+	//spew.Dump(response)
+	// cblogger.Debug(response.ToJsonString())
+	callogger.Info(call.String(callLogInfo))
+
+	jsonString, errJson := ConvertJsonString(response.Response.InstanceTypeConfigSet)
+	if errJson != nil {
+		cblogger.Error(errJson)
+	}
+	return jsonString, errJson
 }
 
 func (vmSpecHandler *TencentVmSpecHandler) GetOrgVMSpec(Region string, Name string) (string, error) {
-	cblogger.Infof("Start GetOrgVMSpec(Region:[%s], Name:[%s])", Region, Name)
-	return "", nil
+	cblogger.Infof("Start GetOrgVMSpec(ZoneId:[%s], Name:[%s])", Region, Name)
+
+	callogger := call.GetLogger("HISCALL")
+	callLogInfo := call.CLOUDLOGSCHEMA{
+		CloudOS:      call.TENCENT,
+		RegionZone:   vmSpecHandler.Region.Zone,
+		ResourceType: call.VMSPEC,
+		ResourceName: Name,
+		CloudOSAPI:   "DescribeInstanceTypeConfigs()",
+		ElapsedTime:  "",
+		ErrorMSG:     "",
+	}
+
+	request := cvm.NewDescribeInstanceTypeConfigsRequest()
+	request.Filters = []*cvm.Filter{
+		&cvm.Filter{
+			Name:   common.StringPtr("zone"),
+			Values: common.StringPtrs([]string{Region}),
+		},
+	}
+	callLogStart := call.Start()
+	response, err := vmSpecHandler.Client.DescribeInstanceTypeConfigs(request)
+	callLogInfo.ElapsedTime = call.Elapsed(callLogStart)
+
+	if err != nil {
+		callLogInfo.ErrorMSG = err.Error()
+		callogger.Error(call.String(callLogInfo))
+
+		cblogger.Error(err)
+		return "", err
+	}
+
+	//spew.Dump(response)
+	cblogger.Debug(response.ToJsonString())
+	callogger.Info(call.String(callLogInfo))
+
+	if len(response.Response.InstanceTypeConfigSet) > 0 {
+		jsonString, errJson := ConvertJsonString(response.Response.InstanceTypeConfigSet[0])
+		if errJson != nil {
+			cblogger.Error(errJson)
+		}
+		return jsonString, errJson	
+	} else {
+		return "", errors.New("정보를 찾을 수 없습니다")
+	}
 }
 
 //인스턴스 스펙 정보를 추출함
