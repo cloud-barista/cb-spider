133dcd72a3a19772770ba40a3b60f84f718c9fb7
powerkimhub <powerkim@etri.re.kr>
2021-05-30 06:48:50 +0000

    update cloudit test scripts due to updated driver

--- api-runtime/rest-runtime/test/1.full-create-test/cloudit-full_test.sh
+++ api-runtime/rest-runtime/test/1.full-create-test/cloudit-full_test.sh
@@ -8,24 +8,26 @@ echo "##   4. VM: StartVM -> List -> Get -> ListStatus -> GetStatus -> Suspend -
 echo "####################################################################"
 
 echo "####################################################################"
-echo "## 1. VPC: Create -> List -> Get"
+echo "## 1. VPC: Create"
 echo "####################################################################"
-curl -sX POST http://localhost:1024/spider/vpc -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "vpc-01", "IPv4_CIDR": "192.168.0.0/16", "SubnetInfoList": [ { "Name": "Default-VPC-subnet-2", "IPv4_CIDR": "192.168.1.0/24"} ] } }' |json_pp
+curl -sX POST http://localhost:1024/spider/vpc -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "vpc-01", "IPv4_CIDR": "192.168.0.0/16", "SubnetInfoList": [ { "Name": "subnet-01", "IPv4_CIDR": "192.168.1.0/24"} ] } }' |json_pp
 echo "#-----------------------------"
 
 echo "####################################################################"
-echo "## 2. SecurityGroup: Create -> List -> Get"
+echo "## 2. SecurityGroup: Create"
 echo "####################################################################"
 curl -sX POST http://localhost:1024/spider/securitygroup -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "sg-01", "VPCName": "vpc-01", "SecurityRules": [ {"FromPort": "1", "ToPort" : "65535", "IPProtocol" : "tcp", "Direction" : "inbound"} ] } }' |json_pp
 echo "#-----------------------------"
 
 echo "####################################################################"
-echo "## 3. KeyPair: CLOUDIT does not support."
+echo "## 3. KeyPair: Create"
 echo "####################################################################"
+curl -sX POST http://localhost:1024/spider/keypair -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "keypair-01" } }' |json_pp
 echo "#-----------------------------"
 
+
 echo "####################################################################"
-echo "## 4. VM: StartVM -> List -> Get -> ListStatus -> GetStatus -> Suspend -> Resume -> Reboot"
+echo "## 4. VM: StartVM"
 echo "####################################################################"
-curl -sX POST http://localhost:1024/spider/vm -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "'${CONN_CONFIG}'-vm-01", "ImageName": "'${IMAGE_NAME}'", "VPCName": "vpc-01", "SubnetName": "Default-VPC-subnet-2", "SecurityGroupNames": [ "sg-01" ], "VMSpecName": "'${SPEC_NAME}'", "VMUserId": "root", "VMUserPasswd": "etriETRI!@" } }' |json_pp
+curl -sX POST http://localhost:1024/spider/vm -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "'${CONN_CONFIG}'-vm-01", "ImageName": "'${IMAGE_NAME}'", "VPCName": "vpc-01", "SubnetName": "subnet-01", "SecurityGroupNames": [ "sg-01" ], "VMSpecName": "'${SPEC_NAME}'", "KeyPairName": "keypair-01"}, "VMUserId": "root", "VMUserPasswd": "etriETRI!@" } }' |json_pp
 
--- api-runtime/rest-runtime/test/4.full-delete-test/cloudit-full_test.sh
+++ api-runtime/rest-runtime/test/4.full-delete-test/cloudit-full_test.sh
@@ -11,12 +11,10 @@ echo "####################################################################"
 echo "## 4. VM: Terminate(Delete)"
 echo "####################################################################"
 curl -sX DELETE http://localhost:1024/spider/vm/${CONN_CONFIG}-vm-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
-echo "============== sleep 70 after delete VM"
-sleep 70 
-
 echo "####################################################################"
-echo "## 3. KeyPair: CLOUDIT does not support."
+echo "## 3. KeyPair: Delete"
 echo "####################################################################"
+curl -sX DELETE http://localhost:1024/spider/keypair/keypair-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
 echo "####################################################################"
 echo "## 2. SecurityGroup: Delete"
 echo "####################################################################"
