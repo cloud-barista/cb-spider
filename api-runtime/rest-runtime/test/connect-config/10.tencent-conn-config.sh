99a0d9a11eb823a31fff53807eb3d6ea92f45fff
dev4unet <dev4unet@naver.com>
2021-06-07 17:33:23 +0900

    tencent-conn 샘플 수정

--- api-runtime/rest-runtime/test/connect-config/10.tencent-conn-config.sh
+++ api-runtime/rest-runtime/test/connect-config/10.tencent-conn-config.sh
@@ -4,7 +4,7 @@ RESTSERVER=localhost
 curl -X POST http://$RESTSERVER:1024/spider/driver -H 'Content-Type: application/json' -d '{"DriverName":"tencent-driver01","ProviderName":"TENCENT", "DriverLibFileName":"tencent-driver-v1.0.so"}'
 
  # for Cloud Credential Info
-curl -X POST http://$RESTSERVER:1024/spider/credential -H 'Content-Type: application/json' -d '{"CredentialName":"tencent-credential01","ProviderName":"TENCENT", "KeyValueInfoList": [{"Key":"ClientId", "Value":"AKIDg8JEwNQ12pKaaet8YhbFdlBM2f6mbWaZ"}, {"Key":"ClientSecret", "Value":"2use6MQJFzbM6iJDtcASWNn3FD0Yv42l"}]}'
+curl -X POST http://$RESTSERVER:1024/spider/credential -H 'Content-Type: application/json' -d '{"CredentialName":"tencent-credential01","ProviderName":"TENCENT", "KeyValueInfoList": [{"Key":"ClientId", "Value":"XXXX"}, {"Key":"ClientSecret", "Value":"XXXX"}]}'
 
  # Cloud Region Info for Shooter
 curl -X POST http://$RESTSERVER:1024/spider/region -H 'Content-Type: application/json' -d '{"RegionName":"tencent-tokyo","ProviderName":"TENCENT", "KeyValueInfoList": [{"Key":"Region", "Value":"ap-tokyo"}, {"Key":"Zone", "Value":"ap-tokyo-2"}]}'
