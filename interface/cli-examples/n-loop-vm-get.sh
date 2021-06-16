c0a902facc468cbf0bf22bdf3182b289484571d2
powerkimhub <powerkim@etri.re.kr>
2021-06-16 02:19:50 +0000

    Rename example scripts of cli

--- interface/cli-examples/n-loop-vm-get.sh
+++ interface/cli-examples/n-loop-vm-get.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+        echo
+        echo -e 'usage: '$0' aws|gcp|alibaba|azure|openstack|cloudit # of VMs'
+        echo -e '\n\tex) '$0' aws 10'
+        echo
+        exit 0;
+fi
+
 source ./setup.env $1 $2
 
 max=$2
 ORG_VM_NAME=${VM_NAME}
-
-source ./setup.env $1
 
 while true
 do
@@ -15,8 +21,8 @@ do
 		VM_NAME=${ORG_VM_NAME}-${num}
 
 		time $CLIPATH/spctl --config $CLIPATH/grpc_conf.yaml --cname "${CONN_CONFIG}" vm get -n "${VM_NAME}" 2> /dev/null |grep vm
-		sleep 10
+		#sleep 1
 	done
 	echo
-	sleep 1
+	sleep 2
 done
