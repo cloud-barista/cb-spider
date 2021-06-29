627209aa1e1fd3bad38463318aea20d3cfd047e3
powerkimhub <powerkim@etri.re.kr>
2021-06-29 06:07:33 +0000

    Add REST API test scripts of AddSubnet and RemoveSubnet

--- api-runtime/rest-runtime/test/each-test/vpc-test.sh
+++ api-runtime/rest-runtime/test/each-test/vpc-test.sh
@@ -8,3 +8,11 @@ curl -sX POST http://localhost:1024/spider/vpc -H 'Content-Type: application/jso
 curl -sX GET http://localhost:1024/spider/vpc -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
 curl -sX GET http://localhost:1024/spider/vpc/vpc-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
 
+#### AddSubnet
+curl -sX POST http://localhost:1024/spider/vpc/vpc-01/subnet -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'", "ReqInfo": { "Name": "subnet-02", "IPv4_CIDR": "192.168.2.0/24" } }' |json_pp
+curl -sX GET http://localhost:1024/spider/vpc/vpc-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+
+#### RemSubnet
+curl -sX DELETE http://localhost:1024/spider/vpc/vpc-01/subnet/subnet-02 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+curl -sX GET http://localhost:1024/spider/vpc/vpc-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+
