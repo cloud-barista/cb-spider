75bedf137106154554fd0cffd3d1036f9eea6ed4
dev4unet <dev4unet@naver.com>
2021-08-12 16:52:12 +0900

    common에 PEM에서 Public Key 추출을 위한 MakePublicKeyFromPrivateKey 함수 추가

--- cloud-control-manager/cloud-driver/common/common.go
+++ cloud-control-manager/cloud-driver/common/common.go
@@ -9,60 +9,79 @@
 package common
 
 import (
-        "crypto/rand"
-        "crypto/rsa"
-        "crypto/x509"
-        "encoding/pem"
-        "io/ioutil"
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
 
-        "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
 // generate a KeyPair with 4KB length
 // returns: privateKeyBytes, publicKeyBytes, error
 func GenKeyPair() ([]byte, []byte, error) {
 
-        // (1) Generate a private Key
-        keyLength := 4096
-        privateKey, err := rsa.GenerateKey(rand.Reader, keyLength)
-        if err != nil {
-                return nil, nil, err
-        }
+	// (1) Generate a private Key
+	keyLength := 4096
+	privateKey, err := rsa.GenerateKey(rand.Reader, keyLength)
+	if err != nil {
+		return nil, nil, err
+	}
 
-        err = privateKey.Validate()
-        if err != nil {
-                return nil, nil, err
-        }
+	err = privateKey.Validate()
+	if err != nil {
+		return nil, nil, err
+	}
 
-        // for ASN.1 DER format
-        DERKey := x509.MarshalPKCS1PrivateKey(privateKey)
-        keyBlock := pem.Block{
-                Type:    "RSA PRIVATE KEY",
-                Headers: nil,
-                Bytes:   DERKey,
-        }
+	// for ASN.1 DER format
+	DERKey := x509.MarshalPKCS1PrivateKey(privateKey)
+	keyBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   DERKey,
+	}
 
-        // for PEM format
-        privateKeyBytes := pem.EncodeToMemory(&keyBlock)
+	// for PEM format
+	privateKeyBytes := pem.EncodeToMemory(&keyBlock)
 
-
-        // (2) Generate a public key
+	// (2) Generate a public key
 	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
-        if err != nil {
-                return nil, nil, err
-        }
+	if err != nil {
+		return nil, nil, err
+	}
 
-        publicKeyBytes := ssh.MarshalAuthorizedKey(publicKey)
+	publicKeyBytes := ssh.MarshalAuthorizedKey(publicKey)
 
-        return privateKeyBytes, publicKeyBytes, nil
+	return privateKeyBytes, publicKeyBytes, nil
 }
 
 // save a key to a file
 func SaveKey(keyBytes []byte, targetFile string) error {
-        err := ioutil.WriteFile(targetFile, keyBytes, 0600)
-        if err != nil {
-                return err
-        }
+	err := ioutil.WriteFile(targetFile, keyBytes, 0600)
+	if err != nil {
+		return err
+	}
 
-        return nil
+	return nil
+}
+
+// ParseKey reads the given RSA private key and create a public one for it.
+func MakePublicKeyFromPrivateKey(pem string) (string, error) {
+	key, err := ssh.ParseRawPrivateKey([]byte(pem))
+	if err != nil {
+		return "", err
+	}
+	rsaKey, ok := key.(*rsa.PrivateKey)
+	if !ok {
+		return "", fmt.Errorf("%q is not a RSA key", pem)
+	}
+	pub, err := ssh.NewPublicKey(&rsaKey.PublicKey)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes.TrimRight(ssh.MarshalAuthorizedKey(pub), "\n")), nil
 }
