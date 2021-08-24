edcbcc30a96b54ece8ae6882122df4235b0590e7
powerkimhub <powerkim@etri.re.kr>
2021-08-24 11:57:02 +0000

    add test scripts for REST API Gateway

--- api-runtime/rest-runtime/test/spec-test/1.aws-get-test-for-gateway.sh
+++ api-runtime/rest-runtime/test/spec-test/1.aws-get-test-for-gateway.sh
@@ -1,5 +1,4 @@
-#export GATEWAY_HOST=1.2.3.4
-export GATEWAY_HOST=52.197.30.208
+export GATEWAY_HOST=1.2.3.4
 export CONN_CONFIG=aws-ohio-config
 export IMAGE_NAME=ami-f4f4cf91
 export SPEC_NAME=t3.micro
