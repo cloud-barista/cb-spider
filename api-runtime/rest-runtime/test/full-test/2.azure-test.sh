639ff6d800c04a0d14e40fa357095d5d3f82c027
powerkimhub <powerkim@etri.re.kr>
2021-09-29 09:05:41 +0000

    enhance some REST API test scripts

--- api-runtime/rest-runtime/test/full-test/2.azure-test.sh
+++ api-runtime/rest-runtime/test/full-test/2.azure-test.sh
@@ -1,5 +1,6 @@
 export CONN_CONFIG=azure-northeu-config
-export IMAGE_NAME=Canonical:UbuntuServer:18.04-LTS:latest
+# Now, latest is not supported like 'GetImage("Canonical:UbuntuServer:18.04-LTS:latest")' // export IMAGE_NAME=Canonical:UbuntuServer:18.04-LTS:latest
+export IMAGE_NAME=Canonical:UbuntuServer:18.04-LTS:18.04.202109180
 export SPEC_NAME=Standard_B1ls
 
 ./full_test.sh
--- api-runtime/rest-runtime/test/full-test/3.aws-desvstack-openstack-test.sh
+++
@@ -1,5 +0,0 @@
-export CONN_CONFIG=openstack-config01
-export IMAGE_NAME=cirros-0.5.1-x86_64-disk
-export SPEC_NAME=m1.micro
-
-./full_test.sh
