7a4eeeaa2510883f4c015fa5c3a3f8e248b9aecf
powerkimhub <powerkim@etri.re.kr>
2021-11-12 04:27:06 +0000

    Change the imageID to test get image info from Azure

--- api-runtime/rest-runtime/test/image-test/2.azure-get-test.sh
+++ api-runtime/rest-runtime/test/image-test/2.azure-get-test.sh
@@ -1,4 +1,4 @@
 export CONN_CONFIG=azure-northeu-config
-export IMAGE_NAME=Canonical:UbuntuServer:18.04-LTS:latest
+export IMAGE_NAME=Canonical:UbuntuServer:18.04-LTS:18.04.202106220
 
 ./image-get-test.sh
