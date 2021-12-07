28965112d13105d7d224915f6beaac1fe3427fec
powerkimhub <powerkim@etri.re.kr>
2021-12-07 10:49:43 +0000

    Fix the test scenario of driver common API

--- cloud-control-manager/cloud-driver/common/test/keypair_manage_test.go
+++ cloud-control-manager/cloud-driver/common/test/keypair_manage_test.go
@@ -28,12 +28,14 @@ func TestAddListGetDelete(t *testing.T) {
 
 
 	// (1) insert-1
+        log.Println("====================================== (1) insert-1 ======================================")
 	err = cdcom.AddKey("CLOUDIT", strHash, keyPairNameId, string(privateKey))
 	if err != nil {
 		log.Fatal("something failed!")
 	}
 
 	// (1) insert-2
+        log.Println("====================================== (1) insert-2 ======================================")
 	privateKey, _, err = cdcom.GenKeyPair()
         keyPairNameId = "keypair-1-c6ncl9aba5o081np93og"
 	err = cdcom.AddKey("CLOUDIT", strHash, keyPairNameId, string(privateKey))
@@ -43,14 +45,19 @@ func TestAddListGetDelete(t *testing.T) {
 
 
 	// (2) list
+        log.Println("====================================== (2) list ======================================")
 	keyValueList, err := cdcom.ListKey("CLOUDIT", strHash)
         if err != nil {
                 log.Fatal("something failed!")
         }
 	log.Println(keyValueList)
+	if len(keyValueList) != 2 {
+                log.Fatal("The number of Key list is not 2!")
+	}
 
 
 	// (3) get
+        log.Println("====================================== (3) get ======================================")
 	keyValue, err := cdcom.GetKey("CLOUDIT", strHash, keyPairNameId)
         if err != nil {
                 log.Fatal("something failed!")
@@ -58,10 +65,31 @@ func TestAddListGetDelete(t *testing.T) {
         log.Println(keyValue)
 
 
-	// (4) delete
+	// (4) delete-1
+        log.Println("====================================== (4) delete-1 ======================================")
+        keyPairNameId = "keypair-0-c6ncl9aba5o081np93og"
         err = cdcom.DelKey("CLOUDIT", strHash, keyPairNameId)
         if err != nil {
                 log.Fatal("something failed!")
         }
+
+	// (4) delete-2
+        log.Println("====================================== (4) delete-2 ======================================")
+        keyPairNameId = "keypair-1-c6ncl9aba5o081np93og"
+        err = cdcom.DelKey("CLOUDIT", strHash, keyPairNameId)
+        if err != nil {
+                log.Fatal("something failed!")
+        }
+
+	// (5) check
+        log.Println("====================================== (5) check ======================================")
+        keyValueList, err = cdcom.ListKey("CLOUDIT", strHash)
+        if err != nil {
+                log.Fatal("something failed!")
+        }
+        log.Println(keyValueList)
+	if len(keyValueList) != 0 {
+                log.Fatal("The number of Key list is not 0!")
+	}
 }
 
