9e0de024b710e65a021de3be10d69129adfa77d8
tjeom-inno <tj5261@innogrid.com>
2022-02-22 14:28:21 +0900

    [Cloudit] KeyPair - Enhance handling of local SSH Key files
    - Enhance handling of local SSH Key files 이슈 적용 #548
    - KeyPair 리턴 형식 변경 
      - Return info of the KeyPair Create/List/Get API are not clear #560

--- cloud-control-manager/cloud-driver/drivers/cloudit/resources/KeyPairHandler.go
+++ cloud-control-manager/cloud-driver/drivers/cloudit/resources/KeyPairHandler.go
@@ -2,19 +2,17 @@ package resources
 
 import (
 	"errors"
-	"fmt"
+	call "github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/call-log"
 	keypair "github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/common"
-	"io/ioutil"
-	"os"
-	"strings"
-
 	"github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/drivers/cloudit/client"
 	idrv "github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/interfaces"
 	irs "github.com/cloud-barista/cb-spider/cloud-control-manager/cloud-driver/interfaces/resources"
+	"os"
 )
 
 const (
-	KeyPair = "KEYPAIR"
+	KeyPair         = "KEYPAIR"
+	KeyPairProvider = "cloudit"
 )
 
 type ClouditKeyPairHandler struct {
@@ -33,7 +31,8 @@ func (keyPairHandler *ClouditKeyPairHandler) CheckKeyPairFolder(folderPath strin
 }
 
 func (keyPairHandler *ClouditKeyPairHandler) CreateKey(keyPairReqInfo irs.KeyPairReqInfo) (irs.KeyPairInfo, error) {
-
+	hiscallInfo := GetCallLogScheme(ClouditRegion, call.VMKEYPAIR, keyPairReqInfo.IId.NameId, "CreateKey()")
+	start := call.Start()
 	keyPairPath := os.Getenv("CBSPIDER_ROOT") + CBKeyPairPath
 	if err := keyPairHandler.CheckKeyPairFolder(keyPairPath); err != nil {
 		cblogger.Error(err)
@@ -42,38 +41,29 @@ func (keyPairHandler *ClouditKeyPairHandler) CreateKey(keyPairReqInfo irs.KeyPai
 	hashString, err := CreateHashString(keyPairHandler.CredentialInfo)
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
+		return irs.KeyPairInfo{}, err
+	}
+	existKey, _ := keypair.GetKey(KeyPairProvider, hashString, keyPairReqInfo.IId.NameId)
+	if existKey != nil {
+		cblogger.Error(errors.New("exist Key"))
+		LoggingError(hiscallInfo, err)
 		return irs.KeyPairInfo{}, err
 	}
 
-	savePrivateFileTo := keyPairPath + hashString + "--" + keyPairReqInfo.IId.NameId
-	savePublicFileTo := keyPairPath + hashString + "--" + keyPairReqInfo.IId.NameId + ".pub"
+	privateKeyBytes, _, err := keypair.GenKeyPair()
 
-	// Check KeyPair Exists
-	if _, err := os.Stat(savePrivateFileTo); err == nil {
-		errMsg := fmt.Sprintf("KeyPair with name %s already exist", keyPairReqInfo.IId.NameId)
-		createErr := errors.New(errMsg)
-		cblogger.Error(createErr.Error())
-		return irs.KeyPairInfo{}, createErr
-	}
-
-	// 지정된 바이트크기의 RSA 형식 개인키(비공개키)를 만듬
-	privateKey, publicKey, err := keypair.GenKeyPair()
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return irs.KeyPairInfo{}, err
 	}
 
-	// 파일에 private Key를 쓴다
-	err = keypair.SaveKey(privateKey, savePrivateFileTo)
-	if err != nil {
-		cblogger.Error(err)
-		return irs.KeyPairInfo{}, err
-	}
+	err = keypair.AddKey(KeyPairProvider, hashString, keyPairReqInfo.IId.NameId, string(privateKeyBytes))
 
-	// 파일에 public Key를 쓴다
-	err = keypair.SaveKey([]byte(publicKey), savePublicFileTo)
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return irs.KeyPairInfo{}, err
 	}
 
@@ -82,87 +72,86 @@ func (keyPairHandler *ClouditKeyPairHandler) CreateKey(keyPairReqInfo irs.KeyPai
 			NameId:   keyPairReqInfo.IId.NameId,
 			SystemId: keyPairReqInfo.IId.NameId,
 		},
-		PublicKey:  string(publicKey),
-		PrivateKey: string(privateKey),
+		VMUserID: SSHDefaultUser,
+		//PublicKey:  string(publicKeyBytes),
+		PrivateKey: string(privateKeyBytes),
 	}
+	LoggingInfo(hiscallInfo, start)
 	return keyPairInfo, nil
 }
 
 func (keyPairHandler *ClouditKeyPairHandler) ListKey() ([]*irs.KeyPairInfo, error) {
-
+	hiscallInfo := GetCallLogScheme(ClouditRegion, call.VMKEYPAIR, KeyPair, "ListKey()")
+	start := call.Start()
 	keyPairPath := os.Getenv("CBSPIDER_ROOT") + CBKeyPairPath
 	if err := keyPairHandler.CheckKeyPairFolder(keyPairPath); err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return nil, err
 	}
 	hashString, err := CreateHashString(keyPairHandler.CredentialInfo)
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return nil, err
 	}
+
+	keyValueList, err := keypair.ListKey(KeyPairProvider, hashString)
 
 	var keyPairInfoList []*irs.KeyPairInfo
 
-	files, err := ioutil.ReadDir(keyPairPath)
-	if err != nil {
-		cblogger.Error(err)
-		return nil, err
-	}
-
-	for _, f := range files {
-		if strings.Contains(f.Name(), ".pub") {
-			continue
+	for _, keyValue := range keyValueList {
+		//keypairInfo, err := keyPairHandler.GetKey(irs.IID{SystemId: keyValue.Key})
+		keypairInfo := irs.KeyPairInfo{
+			IId: irs.IID{
+				NameId:   keyValue.Key,
+				SystemId: keyValue.Key,
+			},
+			VMUserID: SSHDefaultUser,
+			//PrivateKey: keyValue.Value,
 		}
-		if strings.Contains(f.Name(), hashString) {
-			fileNameArr := strings.Split(f.Name(), "--")
-			keypairInfo, err := keyPairHandler.GetKey(irs.IID{NameId: fileNameArr[1]})
-			if err != nil {
-				return nil, err
-			}
-			keyPairInfoList = append(keyPairInfoList, &keypairInfo)
+		if err != nil {
+			cblogger.Error(err)
+			LoggingError(hiscallInfo, err)
+			return nil, err
 		}
+		keyPairInfoList = append(keyPairInfoList, &keypairInfo)
 	}
-
+	LoggingInfo(hiscallInfo, start)
 	return keyPairInfoList, nil
 }
 
 func (keyPairHandler *ClouditKeyPairHandler) GetKey(keyIID irs.IID) (irs.KeyPairInfo, error) {
+	hiscallInfo := GetCallLogScheme(ClouditRegion, call.VMKEYPAIR, keyIID.NameId, "GetKey()")
+	start := call.Start()
 
-	keyPairPath := os.Getenv("CBSPIDER_ROOT") + CBKeyPairPath
-	if err := keyPairHandler.CheckKeyPairFolder(keyPairPath); err != nil {
-		cblogger.Error(err)
-		return irs.KeyPairInfo{}, err
-	}
 	hashString, err := CreateHashString(keyPairHandler.CredentialInfo)
-
-	privateKeyPath := keyPairPath + hashString + "--" + keyIID.NameId
-	publicKeyPath := keyPairPath + hashString + "--" + keyIID.NameId + ".pub"
-
-	// Private Key, Public Key 파일 정보 가져오기
-	privateKeyBytes, err := ioutil.ReadFile(privateKeyPath)
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return irs.KeyPairInfo{}, err
 	}
-	publicKeyBytes, err := ioutil.ReadFile(publicKeyPath)
+	_, err = keypair.GetKey(KeyPairProvider, hashString, keyIID.NameId)
 	if err != nil {
 		cblogger.Error(err)
+		LoggingError(hiscallInfo, err)
 		return irs.KeyPairInfo{}, err
 	}
-
 	keypairInfo := irs.KeyPairInfo{
 		IId: irs.IID{
 			NameId:   keyIID.NameId,
 			SystemId: keyIID.NameId,
 		},
-		PublicKey:  string(publicKeyBytes),
-		PrivateKey: string(privateKeyBytes),
+		VMUserID: SSHDefaultUser,
+		// PrivateKey: keyValue.Value,
 	}
+	LoggingInfo(hiscallInfo, start)
 	return keypairInfo, nil
 }
 
 func (keyPairHandler *ClouditKeyPairHandler) DeleteKey(keyIID irs.IID) (bool, error) {
-
+	hiscallInfo := GetCallLogScheme(ClouditRegion, call.VMKEYPAIR, keyIID.NameId, "DeleteKey()")
+	start := call.Start()
 	keyPairPath := os.Getenv("CBSPIDER_ROOT") + CBKeyPairPath
 	if err := keyPairHandler.CheckKeyPairFolder(keyPairPath); err != nil {
 		return false, err
@@ -173,20 +162,16 @@ func (keyPairHandler *ClouditKeyPairHandler) DeleteKey(keyIID irs.IID) (bool, er
 		return false, err
 	}
 
-	privateKeyPath := keyPairPath + hashString + "--" + keyIID.NameId
-	publicKeyPath := keyPairPath + hashString + "--" + keyIID.NameId + ".pub"
-
-	// Private Key, Public Key 삭제
-	err = os.Remove(privateKeyPath)
-	if err != nil {
+	keyValue, err := keypair.GetKey(KeyPairProvider, hashString, keyIID.NameId)
+	if keyValue == nil {
+		cblogger.Error("KeyPair Not Found")
+		return false, err
+	}
+	delerr := keypair.DelKey(KeyPairProvider, hashString, keyIID.NameId)
+	if delerr != nil {
 		cblogger.Error(err)
 		return false, err
 	}
-	err = os.Remove(publicKeyPath)
-	if err != nil {
-		cblogger.Error(err)
-		return false, err
-	}
-
+	LoggingInfo(hiscallInfo, start)
 	return true, nil
 }
