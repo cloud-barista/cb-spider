bf0c9bf229cbae7773759e6e4345c409a767742a
powerkimhub <powerkim@etri.re.kr>
2022-04-24 16:42:18 +0000

    Add all inbound test cases for SG rules validation

---
+++ test/sg-rules-validation-cli/1.inbound-rule-test/03.inbound-case-03.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+        echo
+        echo -e 'usage: '$0' mock|aws|azure|gcp|alibaba|tencent|ibm|openstack|cloudit|ncp|nhncloud'
+        echo -e '\n\tex) '$0' aws'
+        echo
+        exit 0;
+fi
+
+echo -e "#############################################"
+echo -e "# start vm => run tcp server & udp server "
+echo -e "#############################################"
+
+source ../common/setup.env $1
+source setup.env $1
+
+echo "============== before AddRules: '${SG_NAME}' --- inbound:TCP/22/22"
+#### @todo Change this command with spctl
+curl -sX POST http://localhost:1024/spider/securitygroup/${SG_NAME}/rules -H 'Content-Type: application/json' -d \
+        '{
+                "ConnectionName": "'${CONN_CONFIG}'",
+                "ReqInfo": {
+                "RuleInfoList" :
+                        [
+                                {
+                                        "Direction": "inbound",
+                                        "IPProtocol": "TCP",
+                                        "FromPort": "22",
+                                        "ToPort": "22"
+                                }
+                        ]
+                }
+        }' |json_pp
+
+echo "============== after AddRules: '${SG_NAME}' --- inbound:TCP/22/22"
+
+sleep 2
+
+# print the table header of test results
+$(test_result_header $1)
+
+
+#                   CSP I:TCP-01 I:TCP-02 I:UDP-01 I:ICMP-01 | O:TCP-01 O:TCP-02 O:UDP-01 O:ICMP-01
+#./io-traffic-test.sh $1    $2      $3        $4       $5           $6       $7       $8      $9
+./io-traffic-test.sh $1    pass    fail      skip     fail         pass     pass     pass    pass
+
+# print the end mesg of test results
+$(test_result_tailer)
+
+
+echo -e "\n\n"
+
