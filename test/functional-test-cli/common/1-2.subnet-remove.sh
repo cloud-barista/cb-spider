64316719fcb5a4333dbcfe9b067da7327812c1ff
powerkimhub <powerkim@etri.re.kr>
2022-07-05 05:44:34 +0000

    Change the add subnet CIDR Info of test script for Cloudit

--- test/functional-test-cli/common/1-2.subnet-remove.sh
+++ test/functional-test-cli/common/1-2.subnet-remove.sh
@@ -16,6 +16,11 @@ SUBNET_NAME=${SUBNET_NAME}-Added
 # SUBNET_CIDR=192.168.0.0/24
 SUBNET_CIDR=`echo $SUBNET_CIDR | sed 's/0\./1\./g'`
 
+# for Cloudit
+if [ $SUBNET_CIDR_ADD ]; then
+        SUBNET_CIDR=$SUBNET_CIDR_ADD
+fi
+
 echo "============== before remove Subnet: '${VPC_NAME}' : '${SUBNET_NAME}'"
 
 $CLIPATH/spctl --config $CLIPATH/spctl.conf vpc remove-subnet --vname ${VPC_NAME} --sname ${SUBNET_NAME} --cname "${CONN_CONFIG}"
