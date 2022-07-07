23d3c9bec5d1116b992924841d86c9ebe58566ac
powerkimhub <powerkim@etri.re.kr>
2022-07-07 10:35:48 +0000

    Support run compatibility on windows or mac

--- api-runtime/rest-runtime/test/nlb-test/common/check-nlb-test.sh
+++ api-runtime/rest-runtime/test/nlb-test/common/check-nlb-test.sh
@@ -1,7 +1,7 @@
-NLB_ADDR=3.20.19.158
+NLB_ADDR=$1
 
 while true
 do
-	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $NLB_ADDR > /dev/null
+	ssh-keygen -f "$HOME/.ssh/known_hosts" -R $NLB_ADDR > /dev/null
 	ssh -i ~/.aws/powerkim-nlb-test.pem -o StrictHostKeyChecking=no $NLB_ADDR hostname 2> /dev/null
 done
