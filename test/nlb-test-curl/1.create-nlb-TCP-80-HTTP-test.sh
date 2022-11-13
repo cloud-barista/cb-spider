3d61f30ec3fa1d2fcb95b2896b9d38dbf614eede
Sean Oh <innodreamer@gmail.com>
2022-11-13 20:14:13 +0900

    Add NCP VPC NLB Test Config.

--- test/nlb-test-curl/1.create-nlb-TCP-80-HTTP-test.sh
+++ test/nlb-test-curl/1.create-nlb-TCP-80-HTTP-test.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "" ]; then
         echo
-        echo -e 'usage: '$0' mock|aws|azure|gcp|alibaba|tencent|ibm|openstack|cloudit|ncp|nhncloud'
+        echo -e 'usage: '$0' mock|aws|azure|gcp|alibaba|tencent|ibm|openstack|cloudit|ncp|nhncloud|ncpvpc|ktvpc'
         echo -e '\n\tex) '$0' aws' 
         echo
         exit 0;
