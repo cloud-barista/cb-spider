87df2f2793c1483f2388cb8b69a85cb782503132
powerkimhub <powerkim@etri.re.kr>
2023-09-13 10:55:44 +0000

    Add initial driver interface for regin and zone info

---
+++ cloud-control-manager/cloud-driver/interfaces/resources/RegionZoneHandler.go
@@ -0,0 +1,41 @@
+// Cloud Driver Interface of CB-Spider.
+// The CB-Spider is a sub-Framework of the Cloud-Barista Multi-Cloud Project.
+// The CB-Spider Mission is to connect all the clouds with a single interface.
+//
+//      * Cloud-Barista: https://github.com/cloud-barista
+//
+// This is Resouces interfaces of Cloud Driver.
+//
+// by CB-Spider Team, 2023.09.
+
+package resources
+
+// -------- Const
+type ZoneStatus string
+
+const (
+	ZoneAvailable   ZoneStatus = "Available"
+	ZoneUnavailable ZoneStatus = "Unavailable"
+)
+
+type RegionZoneInfo struct {
+	Name        string
+	DisplayName string
+	ZoneList    []ZoneInfo
+
+	KeyValueList []KeyValue
+}
+
+type ZoneInfo struct {
+	Name        string
+	DisplayName string
+	Status      ZoneStatus // Available | Unavailable
+
+	KeyValueList []KeyValue
+}
+
+type RegionZoneHandler interface {
+	ListRegionZone() ([]*RegionZoneInfo, error)
+	ListOrgRegion() (string, error) // return string: json format
+	ListOrgZone() (string, error)   // return string: json format
+}
