5950c9f7b22bade9953af40a9725b6ac5931fa1e
Sean Oh <innodreamer@gmail.com>
2023-10-11 22:49:46 +0900

    Add KT Cloud VPC RegionZoneHandler Test Code

---
+++ api-runtime/rest-runtime/test/region-zone-test/15.ktcloudvpc-region-zone-test.sh
@@ -0,0 +1,4 @@
+export CONN_CONFIG=ktcloudvpc-mokdong1-config
+export REGION_NAME=KR1
+
+./region-zone_test.sh
--- api-runtime/rest-runtime/test/region-zone-test/region-zone_test.sh
+++ api-runtime/rest-runtime/test/region-zone-test/region-zone_test.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
 echo "#### Region-Zone Test Process - Start ###"
 
 sleep 1 
 echo -e "\n### Region/Zone : List"
+start=`date +%s.%N`
 curl -sX GET http://localhost:1024/spider/regionzone -H 'Content-Type: application/json' -d '{"ConnectionName": "'${CONN_CONFIG}'"}' | json_pp
+finish=`date +%s.%N`
+diff=$( echo "$finish - $start" | bc -l )
+echo '# start:' $start
+echo '# finish:' $finish
+echo '# diff(sec):' $diff
 
 sleep 2 
 echo -e "\n### Region/Zone : Get"
@@ -10,10 +17,10 @@ curl -sX GET http://localhost:1024/spider/regionzone/${REGION_NAME} -H 'Content-
 
 sleep 2 
 echo -e "\n### ORG Region : List"
-curl -sX GET http://localhost:1024/spider/orgregion -H 'Content-Type: application/json' -d '{"ConnectionName": "'${CONN_CONFIG}'"}' | json_pp 
+curl -sX GET http://localhost:1024/spider/orgregion -H 'Content-Type: application/json' -d '{"ConnectionName": "'${CONN_CONFIG}'"}' | json_pp
 
 sleep 2
 echo -e "\n### ORG Zone : List"
-curl -sX GET http://localhost:1024/spider/orgzone -H 'Content-Type: application/json' -d '{"ConnectionName": "'${CONN_CONFIG}'"}' | json_pp 
+curl -sX GET http://localhost:1024/spider/orgzone -H 'Content-Type: application/json' -d '{"ConnectionName": "'${CONN_CONFIG}'"}' | json_pp
 
 echo "#### Region-Zone Test Process- Finished ###"
