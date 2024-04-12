fbce7f26010d82132c711db5b281e5e385f2478f
dogfootman <idogfootman@gmail.com>
2024-04-12 15:38:33 +0900

    tencent createDisk set zone

--- cloud-control-manager/cloud-driver/drivers/tencent/resources/DiskHandler.go
+++ cloud-control-manager/cloud-driver/drivers/tencent/resources/DiskHandler.go
@@ -43,8 +43,14 @@ func (DiskHandler *TencentDiskHandler) CreateDisk(diskReqInfo irs.DiskInfo) (irs
 		return irs.DiskInfo{}, errors.New("A disk with the name " + diskReqInfo.IId.NameId + " already exists.")
 	}
 
+	// region base이므로 특정 zone을 지정시 해당 zone에 생성.
+	zone := DiskHandler.Region.Zone
+	if diskReqInfo.Zone != ""{
+		zone = diskReqInfo.Zone
+	}
+
 	request := cbs.NewCreateDisksRequest()
-	request.Placement = &cbs.Placement{Zone: common.StringPtr(DiskHandler.Region.Zone)}
+	request.Placement = &cbs.Placement{Zone: common.StringPtr(zone)}
 	request.DiskChargeType = common.StringPtr("POSTPAID_BY_HOUR")
 
 	diskErr := validateDisk(&diskReqInfo)
