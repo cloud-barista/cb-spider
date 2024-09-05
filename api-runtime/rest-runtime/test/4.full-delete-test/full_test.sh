4983446b3ead79c3b1cf6e0f2d3be03f141eb5e9
powerkimhub <powerkim@etri.re.kr>
2024-09-05 22:07:05 +0900

    enhance test scripts format

--- api-runtime/rest-runtime/test/4.full-delete-test/full_test.sh
+++ api-runtime/rest-runtime/test/4.full-delete-test/full_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 echo "####################################################################"
 echo "##   4. VM: Terminate(Delete)"
@@ -6,21 +7,47 @@ echo "##   2. SecurityGroup: Delete"
 echo "##   1. VPC: Delete"
 echo "####################################################################"
 
+# 4. VM: Terminate(Delete)
+echo
 echo "####################################################################"
 echo "## 4. VM: Terminate(Delete)"
 echo "####################################################################"
-curl -sX DELETE http://localhost:1024/spider/vm/${CONN_CONFIG}-vm-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+curl -sX DELETE http://localhost:1024/spider/vm/${CONN_CONFIG}-vm-01 \
+    -H 'Content-Type: application/json' \
+    -d '{ 
+        "ConnectionName": "'${CONN_CONFIG}'" 
+    }' | json_pp
 
+# 3. KeyPair: Delete
+echo
 echo "####################################################################"
 echo "## 3. KeyPair: Delete"
 echo "####################################################################"
-curl -sX DELETE http://localhost:1024/spider/keypair/keypair-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+curl -sX DELETE http://localhost:1024/spider/keypair/keypair-01 \
+    -H 'Content-Type: application/json' \
+    -d '{ 
+        "ConnectionName": "'${CONN_CONFIG}'" 
+    }' | json_pp
+
+# 2. SecurityGroup: Delete
+echo
 echo "####################################################################"
 echo "## 2. SecurityGroup: Delete"
 echo "####################################################################"
-curl -sX DELETE http://localhost:1024/spider/securitygroup/sg-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+curl -sX DELETE http://localhost:1024/spider/securitygroup/sg-01 \
+    -H 'Content-Type: application/json' \
+    -d '{ 
+        "ConnectionName": "'${CONN_CONFIG}'" 
+    }' | json_pp
+
+# 1. VPC: Delete
+echo
 echo "####################################################################"
 echo "## 1. VPC: Delete"
 echo "####################################################################"
-curl -sX DELETE http://localhost:1024/spider/vpc/vpc-01 -H 'Content-Type: application/json' -d '{ "ConnectionName": "'${CONN_CONFIG}'"}' |json_pp
+curl -sX DELETE http://localhost:1024/spider/vpc/vpc-01 \
+    -H 'Content-Type: application/json' \
+    -d '{ 
+        "ConnectionName": "'${CONN_CONFIG}'" 
+    }' | json_pp
 
