af6a957c130e6b02039c7602d598ed7a64bfd1f3
ish <ish@innogrid.com>
2024-09-24 10:34:39 +0900

    IBM: Implement Disk's ListIID()

--- cloud-control-manager/cloud-driver/drivers/ibmcloud-vpc/main/Test_Resources.go
+++ cloud-control-manager/cloud-driver/drivers/ibmcloud-vpc/main/Test_Resources.go
@@ -1269,7 +1269,8 @@ func testDiskHandlerListPrint() {
 	cblogger.Info("5. ChangeDiskSize()")
 	cblogger.Info("6. AttachDisk()")
 	cblogger.Info("7. DetachDisk()")
-	cblogger.Info("8. Exit")
+	cblogger.Info("8. ListIID()")
+	cblogger.Info("9. Exit")
 }
 
 func testDiskHandler(config Config) {
@@ -1381,6 +1382,14 @@ Loop:
 				}
 				cblogger.Info("Finish DetachDisk()")
 			case 8:
+				cblogger.Info("Start ListIID() ...")
+				if listIID, err := diskHandler.ListIID(); err != nil {
+					cblogger.Error(err)
+				} else {
+					spew.Dump(listIID)
+				}
+				cblogger.Info("Finish ListIID()")
+			case 9:
 				cblogger.Info("Exit")
 				break Loop
 			}
