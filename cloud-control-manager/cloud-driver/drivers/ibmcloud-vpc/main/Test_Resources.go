871c50fc7ddcdcc643371800a30575bd6ddc4086
ish <ish@innogrid.com>
2024-10-05 18:46:27 +0900

    IBM: Force set test's log level to info

--- cloud-control-manager/cloud-driver/drivers/ibmcloud-vpc/main/Test_Resources.go
+++ cloud-control-manager/cloud-driver/drivers/ibmcloud-vpc/main/Test_Resources.go
@@ -223,6 +223,7 @@ var cblogger *logrus.Logger
 func init() {
 	// cblog is a global variable.
 	cblogger = cblog.GetLogger("CB-SPIDER")
+	cblog.SetLevel("info")
 }
 
 func readConfigFile() Config {
