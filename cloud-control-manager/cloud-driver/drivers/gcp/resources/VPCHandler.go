b7c331027cb1718a50f9dfa6579f41b0abede1aa
powerkimhub <powerkim@etri.re.kr>
2024-10-08 16:43:19 +0900

    Remove error log as GCP GetVPC() is sometimes called to check existence during VPC creation

--- cloud-control-manager/cloud-driver/drivers/gcp/resources/VPCHandler.go
+++ cloud-control-manager/cloud-driver/drivers/gcp/resources/VPCHandler.go
@@ -436,7 +436,7 @@ func (vVPCHandler *GCPVPCHandler) GetVPC(vpcIID irs.IID) (irs.VPCInfo, error) {
 		callLogInfo.ErrorMSG = err.Error()
 
 		callogger.Info(call.String(callLogInfo))
-		cblogger.Error(err)
+		// cblogger.Error(err) // Call GetVPC during creation to check if the VPC already exists. This situation is not an error.
 		return irs.VPCInfo{}, err
 	}
 	callogger.Info(call.String(callLogInfo))
