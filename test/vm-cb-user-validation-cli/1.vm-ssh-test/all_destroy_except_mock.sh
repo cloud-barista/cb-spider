00f11c1de2275181a3a623c241616d4464fea307
powerkimhub <powerkim@etri.re.kr>
2024-11-15 11:40:12 +0900

    Remove mock in test script

--- test/vm-cb-user-validation-cli/1.vm-ssh-test/all_destroy_except_mock.sh
+++ test/vm-cb-user-validation-cli/1.vm-ssh-test/all_destroy_except_mock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CSPLIST=( mock aws azure gcp alibaba tencent ibm openstack ktcloud ktcloudvpc ncp ncpvpc nhncloud )
+CSPLIST=( aws azure gcp alibaba tencent ibm openstack ktcloud ktcloudvpc ncp ncpvpc nhncloud )
 
 function run() {
     param=$1
