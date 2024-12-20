cb5fb7d00237f285c51da385927f279071a66613
dev4unet <dev4unet@naver.com>
2024-12-20 06:30:19 +0000

    Enhance Supported VM Spec Info (Issue #1396 / Issue #1399)

--- cloud-control-manager/cloud-driver/drivers/aws/resources/VMSpecHandler.go
+++ cloud-control-manager/cloud-driver/drivers/aws/resources/VMSpecHandler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"reflect"
 	"strconv"
+	"strings"
 
 	//sdk2 "github.com/aws/aws-sdk-go-v2"
 	"github.com/aws/aws-sdk-go/aws"
@@ -25,11 +26,42 @@ func ExtractGpuInfo(gpuDeviceInfo *ec2.GpuDeviceInfo) irs.GpuInfo {
 	//cblogger.Info("================")
 	//cblogger.Debug(gpuDeviceInfo)
 
-	gpuInfo := irs.GpuInfo{
-		Count: strconv.FormatInt(*gpuDeviceInfo.Count, 10),
-		Mfr:   *gpuDeviceInfo.Manufacturer,
-		Model: *gpuDeviceInfo.Name,
-		Mem:   strconv.FormatInt(*gpuDeviceInfo.MemoryInfo.SizeInMiB, 10),
+	// gpuInfo := irs.GpuInfo{
+	// 	Count: strconv.FormatInt(*gpuDeviceInfo.Count, 10),
+	// 	Mfr:   strings.ToUpper(*gpuDeviceInfo.Manufacturer),
+	// 	Model: strings.ToUpper(*gpuDeviceInfo.Name),
+	// 	Mem:   strconv.FormatInt(*gpuDeviceInfo.MemoryInfo.SizeInMiB, 10),
+	// }
+
+	// GPU 정보 구조체 초기화
+	gpuInfo := irs.GpuInfo{}
+
+	// Count 설정
+	if gpuDeviceInfo.Count != nil {
+		gpuInfo.Count = strconv.FormatInt(*gpuDeviceInfo.Count, 10)
+	} else {
+		gpuInfo.Count = "-1" // 숫자 값 없을 경우 -1
+	}
+
+	// Manufacturer 설정
+	if gpuDeviceInfo.Manufacturer != nil {
+		gpuInfo.Mfr = strings.ToUpper(*gpuDeviceInfo.Manufacturer)
+	} else {
+		gpuInfo.Mfr = "NA" // 문자열 값 없을 경우 "NA"
+	}
+
+	// Model 설정
+	if gpuDeviceInfo.Name != nil {
+		gpuInfo.Model = strings.ToUpper(*gpuDeviceInfo.Name)
+	} else {
+		gpuInfo.Model = "NA" // 문자열 값 없을 경우 "NA"
+	}
+
+	// MemoryInfo 설정
+	if gpuDeviceInfo.MemoryInfo != nil && gpuDeviceInfo.MemoryInfo.SizeInMiB != nil {
+		gpuInfo.Mem = strconv.FormatInt(*gpuDeviceInfo.MemoryInfo.SizeInMiB, 10)
+	} else {
+		gpuInfo.Mem = "-1" // 숫자 값 없을 경우 -1
 	}
 
 	return gpuInfo
@@ -40,6 +72,10 @@ func ExtractVMSpecInfo(Region string, instanceTypeInfo *ec2.InstanceTypeInfo) ir
 	cblogger.Debugf("ExtractVMSpecInfo : Region:[%s] / SpecName:[%s]", Region, *instanceTypeInfo.InstanceType)
 	//cblogger.Debug(instanceTypeInfo)
 
+	//fmt.Println("=======[EC2 응답] ==========")
+	//spew.Dump(instanceTypeInfo)
+	//fmt.Println("===================")
+
 	vCpuInfo := irs.VCpuInfo{}
 	gpuInfoList := []irs.GpuInfo{}
 
@@ -48,14 +84,21 @@ func ExtractVMSpecInfo(Region string, instanceTypeInfo *ec2.InstanceTypeInfo) ir
 		Region: Region,
 	}
 
+	//Disk 정보 처리 (AMI에서만 정보 조회 가능)
+	vmSpecInfo.Disk = "-1"
+
 	//VCPU 정보 처리 - Count
 	if !reflect.ValueOf(instanceTypeInfo.VCpuInfo.DefaultVCpus).IsNil() {
 		vCpuInfo.Count = strconv.FormatInt(*instanceTypeInfo.VCpuInfo.DefaultVCpus, 10)
+	} else {
+		vCpuInfo.Count = "-1"
 	}
 
 	//VCPU 정보 처리 - Clock
 	if !reflect.ValueOf(instanceTypeInfo.ProcessorInfo.SustainedClockSpeedInGhz).IsNil() {
 		vCpuInfo.Clock = strconv.FormatFloat(*instanceTypeInfo.ProcessorInfo.SustainedClockSpeedInGhz, 'f', 1, 64)
+	} else {
+		vCpuInfo.Clock = "-1"
 	}
 	vmSpecInfo.VCpu = vCpuInfo
 
@@ -72,10 +115,14 @@ func ExtractVMSpecInfo(Region string, instanceTypeInfo *ec2.InstanceTypeInfo) ir
 
 	if !reflect.ValueOf(instanceTypeInfo.InstanceType).IsNil() {
 		vmSpecInfo.Name = *instanceTypeInfo.InstanceType
+	} else {
+		vmSpecInfo.Name = "NA"
 	}
 
 	if !reflect.ValueOf(instanceTypeInfo.MemoryInfo.SizeInMiB).IsNil() {
 		vmSpecInfo.Mem = strconv.FormatInt(*instanceTypeInfo.MemoryInfo.SizeInMiB, 10)
+	} else {
+		vmSpecInfo.Mem = "-1"
 	}
 
 	//KeyValue 목록 처리
