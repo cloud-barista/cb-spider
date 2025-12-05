2cfdb528daad5b37984b50d326bc3d255b773989
Sean Oh <innodreamer@gmail.com>
2025-12-05 18:11:02 +0900

    Add NLB REST API Test codes

---
+++ cloud-control-manager/cloud-driver/drivers/kt/main/REST_API_test/16.list_staticNat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./1.export.env
+
+echo "### OS_LB_API - LB List"
+echo "curl -v $OS_LB_API?command=listLoadBalancers&zoneid=DX-M1&response=json"
+# echo "curl -v $OS_LB_API?command=listLoadBalancers&zoneid=DX-M1&response=json"
+
+curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API?command=listLoadBalancers
+
+# curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API?command=listLoadBalancers&zoneid=DX-M1&response=json
+# 이것도 동일한 결과
+# => &response=json 않붙여도됨.
+
+echo -e "\n"
