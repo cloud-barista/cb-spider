89db5466694a564f29bd68e3499666dee341ba15
Seokho Son <shsongist@gmail.com>
2026-05-18 17:58:17 +0900

    Fix IBM cloud-init ssh user, authkey handling err
    Signed-off-by: Seokho Son <shsongist@gmail.com>

--- cloud-control-manager/cloud-driver/drivers/ibm/resources/VMHandler.go
+++ cloud-control-manager/cloud-driver/drivers/ibm/resources/VMHandler.go
@@ -182,6 +182,7 @@ func (vmHandler *IbmVMHandler) StartVM(vmReqInfo irs.VMReqInfo) (irs.VMInfo, err
 		}
 		userData = string(fileDataCloudInit)
 		userData = strings.ReplaceAll(userData, "{{username}}", CBDefaultVmUserName)
+		userData = strings.ReplaceAll(userData, "{{public_key}}", *key.PublicKey)
 	}
 
 	// 2.Create VM
