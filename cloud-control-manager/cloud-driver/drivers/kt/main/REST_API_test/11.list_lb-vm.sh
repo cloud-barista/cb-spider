bf5b6238aeadce3f324bd8eb88b3768ceca2df48
Sean Oh <innodreamer@gmail.com>
2026-07-13 23:22:10 +0900

    Update NLB REST API test codes

--- cloud-control-manager/cloud-driver/drivers/kt/main/REST_API_test/11.list_lb-vm.sh
+++ cloud-control-manager/cloud-driver/drivers/kt/main/REST_API_test/11.list_lb-vm.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 source ./1.export.env
 
-echo "### OS_LB_API - LB List"
-echo "curl -v $OS_LB_API?command=listLoadBalancerWebServers&loadbalancerid=38288&response=json"
+echo "### OS_LB_API - LB VM List (New API - Since 20251220)"
+echo "curl -v $OS_LB_API/30540a4f-3c1b-4053-932e-e507f2c3d808/servers"
 
-#curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API?command=listLoadBalancerWebServers&loadbalancerid=38288&response=json
-# curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API?command=listLoadBalancerWebServers&loadbalancerid=38288
-#echo -e "\n"
-
-curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API?command=listLoadBalancerWebServers&loadbalancerid=38288&multi_stack_id=11&response=json
+curl -v -H "X-Auth-Token: $OS_TOKEN" $OS_LB_API/30540a4f-3c1b-4053-932e-e507f2c3d808/servers
 
 echo -e "\n"
 
+# (For old NLB API)
 # 온라인 고객센터로부터 안내된 예: 
-# <GET http://14.63.254.107/openstack4kt/service1/loadbalancer/api?command=listLoadBalancerWebServers&loadbalancerid=38288&multi_stack_id=11&response=json,[x-auth-token:"gAAAAABlvniyWJCSabJ6CIIDyOIUpZzYRLan_TTXoNEQWmNr1rR3arorAxuLKOmftY8C3TimVz-oXQxnc1rcB2rbS8OAc2WwiGlFXpLUxp1ewsPyTq38G659m1UzKis9pOFC43HBIG4-slOlc3V7oftq4Gyf5DZqMuxmh-EG7y9bHjAx0UxRbOY"
+# <GET http://14.63.254.107/openstack4kt/service1/loadbalancer/api?command=listLoadBalancerWebServers&loadbalancerid=38288&multi_stack_id=11&response=json,[x-auth-token:"XXXXXXXXXXXXXXXXXXXX"
